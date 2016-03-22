fa66c5500d686635f402e7e9970f57ef8ec53baf
Renato Silveira <renatosnj@gmail.com>
2016-03-22 19:13:57 -0300

    Encapsulamento do código da placa (incompleto)

--- board.js
+++ board.js
@@ -2,178 +2,182 @@
  * Created by renato on 14/03/16.
  */
 "use strict";
-var MODULE = (function () {
 
-    /*
-     * Declaracao de Variaveis globais e Importacoes
-     */
-    // Escrita e leitura de arquivos
-    // Importando Modulo File System para leitura e escrita de arquivos
-    var fs = require('fs'),
-    // Tipo de codificacao para abrir os aquivos na placa
-        fileOptions = {encoding: 'ascii'},
+/*
+ * Declaracao de Variaveis globais e Importacoes
+ */
+// Escrita e leitura de arquivos
+// Importando Modulo File System para leitura e escrita de arquivos
+var fs = require('fs'),
+// Tipo de codificacao para abrir os aquivos na placa
+    fileOptions = {encoding: 'ascii'},
 
-    // Variavel auxiliar de enredecamento para testes fora da placa
-        dirname = __dirname,
-        root = dirname + '/root',
-    //var temp_url = dirname + '/temp.txt'
+// Variavel auxiliar de enredecamento para testes fora da placa
+    dirname = __dirname,
+    root = dirname + '/root',
+//var temp_url = dirname + '/temp.txt'
 
-    // Definicoes dos pinos (Funcao x Pino)
-        sensor = 37,
-    // GPIO's necessarios para habilitar led 13 da placa
-        led = 39,
-        mux = 55;
+// Definicoes dos pinos (Funcao x Pino)
+    sensor = 37,
+// GPIO's necessarios para habilitar led 13 da placa
+    led = 39,
+    mux = 55;
 
 
-    /*
-     *   Definindo metodos privados
-     */
-    var exportGpio = function (gpio_nr) {
-        fs.writeFile(root + '/sys/class/gpio/export', gpio_nr, fileOptions,
-            function (err) {
-                if (err) {
-                    console.log("Erro ao exportar o pino %d, provavelmente ja foi" +
-                        "exportado.", gpio_nr);
-                }
-            });
-    };
-
-    var setGpioDirection = function (gpio_nr, direction) {
-        fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/direction",
-            direction, fileOptions, function (err) {
-                if (err) {
-                    console.log("Could'd set gpio" + gpio_nr + " direction to " +
-                        direction + " - probably gpio not available via sysfs\n" + err);
-                }
+/*
+ *   Definindo metodos privados
+ */
+var exportGpio = function (gpio_nr) {
+    fs.writeFile(root + '/sys/class/gpio/export', gpio_nr, fileOptions,
+        function (err) {
+            if (err) {
+                console.log("Erro ao exportar o pino %d, provavelmente ja foi" +
+                    "exportado.", gpio_nr);
             }
-        );
-    };
+        });
+};
 
-    var setGpioDrive = function (gpio_nr, drive) {
-        fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/drive",
-            drive, fileOptions, function (err) {
-                if (err) {
-                    console.log("Could'd set gpio" + gpio_nr + " drive to " +
-                            /*direction +*/ " - probably gpio not available via sysfs\n" + err);
-                }
+var setGpioDirection = function (gpio_nr, direction) {
+    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/direction",
+        direction, fileOptions, function (err) {
+            if (err) {
+                console.log("Could'd set gpio" + gpio_nr + " direction to " +
+                    direction + " - probably gpio not available via sysfs\n" + err);
             }
-        );
-    };
-
-    var setGpioIn = function (gpio_nr) {
-        setGpioDirection(gpio_nr, 'in');
-    };
-
-    var setGpioOut = function (gpio_nr) {
-        setGpioDirection(gpio_nr, 'out');
-    };
-
-    var setGpioDrive = function (gpio_nr, drive) {
-        fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/drive",
-            drive, fileOptions, function (err) {
-                if (err) {
-                    console.log("Could'd set gpio" + gpio_nr + " drive to " +
-                            /*direction +*/ " - probably gpio not available via sysfs\n" + err);
-                }
-            }
-        );
-    };
-
-    /**
-     * Define a intensidade??? de um GPIO como strong
-     * @param {string} gpio_nr - O numero do GPIO
-     */
-    var setGpioStrong = function (gpio_nr) {
-        setGpioDrive(gpio_nr, 'strong');
-    };
-
-    var board = {};
-    /**
-     * Le o valor de um Sensor
-     * @param {string} gpio_nr - O numero do GPIO
-     * @param {function} callback - Uma funcao que se utilizara do valor do Sensor que sera passado por parametro
-     * @return {string} Valor do sensor
-     */
-    board.readSensor = function (gpio_nr, callback) {
-        var value,
-            url;
-        if (root === "") {
-        } else {
-            url = root + "/sys/class/gpio/gpio" + gpio_nr + "/value";
         }
-        fs.readFile(url, fileOptions,
-            function (err, data) {
-                if (err) {
-                    console.log("Error reading gpio" + gpio_nr);
-                }
-                value = data;
-                callback(data);
+    );
+};
+
+var setGpioDrive = function (gpio_nr, drive) {
+    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/drive",
+        drive, fileOptions, function (err) {
+            if (err) {
+                console.log("Could'd set gpio" + gpio_nr + " drive to " +
+                        /*direction +*/ " - probably gpio not available via sysfs\n" + err);
             }
-        );
-        return value;
-    };
+        }
+    );
+};
 
-    /**
-     * Escreve o valor de um GPIO
-     * @param {string} gpio_nr - O numero do GPIO
-     * @param {string} value - Valor  para ser escrito no GPIO
-     */
-    board.writeGpio = function (gpio_nr, value) {
-        fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/value", value, fileOptions,
-            function (err, data) {
-                if (err) {
-                    console.log("Writing " + gpio_nr + " " + value + "\n" + err);
-                }
+var setGpioIn = function (gpio_nr) {
+    setGpioDirection(gpio_nr, 'in');
+};
+
+var setGpioOut = function (gpio_nr) {
+    setGpioDirection(gpio_nr, 'out');
+};
+
+var setGpioDrive = function (gpio_nr, drive) {
+    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/drive",
+        drive, fileOptions, function (err) {
+            if (err) {
+                console.log("Could'd set gpio" + gpio_nr + " drive to " +
+                        /*direction +*/ " - probably gpio not available via sysfs\n" + err);
             }
-        );
-    };
+        }
+    );
+};
 
-    /**
-     * Le o valor de um GPIO
-     * @param {string} gpio_nr - O numero do GPIO
-     * @param {function} callback - Uma funcao que se utilizara do valor do FPIO que sera passado por parametro
-     * @return {string} Valor do GPIO
-     */
-    board.readGpio = function (gpio_nr, callback) {
-        var value = "";
-        fs.readFile("/sys/class/gpio/gpio" + gpio_nr + "/value", fileOptions,
-            function (err, data) {
-                if (err) {
-                    console.log("Error reading gpio" + gpio_nr);
-                }
-                value = data;
-                callback(data);
+/**
+ * Define a intensidade??? de um GPIO como strong
+ * @param {string} gpio_nr - O numero do GPIO
+ */
+var setGpioStrong = function (gpio_nr) {
+    setGpioDrive(gpio_nr, 'strong');
+};
+
+/**
+ * Le o valor de um Sensor
+ * @param {string} gpio_nr - O numero do GPIO
+ * @param {function} callback - Uma funcao que se utilizara do valor do Sensor que sera passado por parametro
+ * @return {string} Valor do sensor
+ */
+exports.readSensor = function (gpio_nr, callback) {
+    var value,
+        url;
+    if (root === "") {
+    } else {
+        url = root + "/sys/class/gpio/gpio" + gpio_nr + "/value";
+    }
+    fs.readFile(url, fileOptions,
+        function (err, data) {
+            if (err) {
+                console.log("Error reading gpio" + gpio_nr);
             }
-        );
-        return value;
-    };
+            value = data;
+            callback(data);
+        }
+    );
+    return value;
+};
 
-    /**
-     * Executa os passos necessarios para inicializar a placa corretamente.
-     */
-    board.inicializarPlaca = function () {
-        // Configurando MUX
-        exportGpio(mux);
-        setGpioOut(mux);
-        setGpioStrong(mux);
-        writeGpio(mux, '1');
+/**
+ * Escreve o valor de um GPIO
+ * @param {string} gpio_nr - O numero do GPIO
+ * @param {string} value - Valor  para ser escrito no GPIO
+ */
+exports.writeGpio = function (gpio_nr, value) {
+    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/value", value, fileOptions,
+        function (err, data) {
+            if (err) {
+                console.log("Writing " + gpio_nr + " " + value + "\n" + err);
+            }
+        }
+    );
+};
 
-        // Configurando LED
-        exportGpio(led);
-        setGpioOut(led);
-        writeGpio(led, '0');
+/**
+ * Le o valor de um GPIO
+ * @param {string} gpio_nr - O numero do GPIO
+ * @param {function} callback - Uma funcao que se utilizara do valor do FPIO que sera passado por parametro
+ * @return {string} Valor do GPIO
+ */
+exports.readGpio = function (gpio_nr, callback) {
+    var value = "";
+    fs.readFile("/sys/class/gpio/gpio" + gpio_nr + "/value", fileOptions,
+        function (err, data) {
+            if (err) {
+                console.log("Error reading gpio" + gpio_nr);
+            }
+            value = data;
+            callback(data);
+        }
+    );
+    return value;
+};
 
-        // Configurando Sensor
-        exportGpio(sensor);
-        setGpioOut(sensor);
-        setGpioStrong(sensor);
-        writeGpio(sensor, "0");
+/**
+ * Executa os passos necessarios para inicializar a placa corretamente.
+ */
+exports.inicializarPlaca = function () {
+    // Configurando MUX
+    exportGpio(mux);
+    setGpioOut(mux);
+    setGpioStrong(mux);
+    exports.writeGpio(mux, '1');
 
-        console.log("Placa configurada com sucesso.");
-    };
-    board.test = function () {
-        console.log("Funcionou")
-    };
+    // Configurando LED
+    exportGpio(led);
+    setGpioOut(led);
+    exports.writeGpio(led, '0');
 
-    return board;
-}());
+    // Configurando Sensor
+    exportGpio(sensor);
+    setGpioOut(sensor);
+    setGpioStrong(sensor);
+    exports.writeGpio(sensor, "0");
+
+    console.log("Placa configurada com sucesso.");
+};
+exports.ligarLed = function () {
+    exports.writeGpio(led, '1');
+    return "Ligando LED da Galileu";
+};
+exports.desligarLed = function () {
+    exports.writeGpio(led, '0');
+    return "Desligando LED da Galileu";
+};
+exports.lerSensor = function (callback){
+    var valor = exports.readSensor(sensor,callback);
+    console.log(valor);
+};
--- server.js
+++ server.js
@@ -5,143 +5,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var fs = require('fs');
-
-// Tipo de codificacao ao abrir os aquivos na placa
-var fileOptions = {encoding: 'ascii'};
+var board = require('./board.js');
 
 // Variavel auxiliar para testes fora da placa
 var dirname = __dirname;
-var root = dirname + '/root';
-//var temp_url = dirname + '/temp.txt';
-
-/******************************************************************************/
-/*            FUNCOES RELACIONADAS A ESCRITA DE ARQUIVOS NA PLACA             */
-/******************************************************************************/
-
-var exportGpio = function (gpio_nr) {
-    fs.writeFile(root + '/sys/class/gpio/export', gpio_nr, fileOptions,
-        function (err) {
-            if (err) {
-                console.log("Erro ao exportar o pino %d, provavelmente ja foi" +
-                    "exportado.", gpio_nr);
-            }
-        });
-};
-
-var setGpioDirection = function (gpio_nr, direction) {
-    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/direction",
-        direction, fileOptions, function (err) {
-            if (err) {
-                console.log("Could'd set gpio" + gpio_nr + " direction to " +
-                    direction + " - probably gpio not available via sysfs\n" + err);
-            }
-        }
-        );
-};
-
-var setGpioIn = function (gpio_nr) {
-    setGpioDirection(gpio_nr, 'in');
-};
-
-var setGpioOut = function (gpio_nr) {
-    setGpioDirection(gpio_nr, 'out');
-};
-
-var setGpioDrive = function (gpio_nr, drive) {
-    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/drive",
-        drive, fileOptions, function (err) {
-            if (err) {
-                console.log("Could'd set gpio" + gpio_nr + " drive to " +
-                    /*direction +*/ " - probably gpio not available via sysfs\n" + err);
-            }
-        }
-        );
-};
-
-var setGpioStrong = function (gpio_nr) {
-    setGpioDrive(gpio_nr, 'strong');
-};
-
-
-// pass callback to process data asynchroniously
-var readGpio = function (gpio_nr, callback) {
-    var value = "";
-    fs.readFile("/sys/class/gpio/gpio" + gpio_nr + "/value", fileOptions,
-        function (err, data) {
-            if (err) {
-                console.log("Error reading gpio" + gpio_nr);
-            }
-            value = data;
-            callback(data);
-        }
-        );
-    return value;
-};
-
-var writeGpio = function (gpio_nr, value) {
-    fs.writeFile(root + "/sys/class/gpio/gpio" + gpio_nr + "/value", value, fileOptions,
-        function (err, data) {
-            if (err) {
-                console.log("Writing " + gpio_nr + " " + value + "\n" + err);
-            }
-        }
-    );
-};
-
-// Nomeando pinos da placa
-var sensor = 37;
-// GPIO's necessarios para habilitar led 13 da placa
-var led = 39;
-var mux = 55;
-var configurarRecursosDaPlaca = function () {
-    // Configurando MUX
-    exportGpio(mux);
-    setGpioOut(mux);
-    setGpioStrong(mux);
-    writeGpio(mux, '1');
-
-    // Configurando LED
-    exportGpio(led);
-    setGpioOut(led);
-    writeGpio(led, '0');
-
-    // Configurando Sensor
-    exportGpio(sensor);
-    setGpioOut(sensor);
-    setGpioStrong(sensor);
-    writeGpio(sensor, '0');
-
-    console.log("Placa configurada com sucesso.");
-}
-
-
-
-var readSensor = function (gpio_nr, callback) {
-    var value;
-    var url;
-    if (root === "") {
-    } else {
-        url = root + "/sys/class/gpio/gpio" + gpio_nr + "/value";
-    }    
-    
-    fs.readFile(url, fileOptions, 
-        function (err, data) {
-            if (err) {
-                console.log("Error reading gpio" + gpio_nr);
-            }
-            value = data;
-            callback(data);
-        }
-    );
-    return value;
-};
-
 /******************************************************************************/
 /*                       CONFIGURACOES DO SERVIDOR                            */
 /******************************************************************************/
-
-configurarRecursosDaPlaca();
 
 // Retorno da pagina inicial
 app.get('/', function (req, res) {
@@ -176,16 +46,16 @@ io.on('connection', function (socket) {
         
         //Executand comando correspondente
         if (comando === "galileu ligar") {
-            writeGpio(led, '1');
+            board.ligarLed();
             io.emit('Resposta da Galileo', "Ligando LED da Galileu");
             
         } else if (comando === "galileu desligar") {
-            writeGpio(led, '0');
+            board.desligarLed();
             io.emit('Resposta da Galileo', "Desligando LED da Galileu");
             
         } else if (comando === "galileu temperatura") {
-            var data = readSensor(sensor, function(valor){
-                io.emit('Resposta da Galileo', "A temperatura atual é " + valor 
+            board.lerSensor(function(valor){
+                io.emit('Resposta da Galileo', "A temperatura atual é " + valor
                     + " graus.");
             });
             
